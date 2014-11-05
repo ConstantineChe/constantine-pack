1b99ac8b6585af6c85c3722af6f8f9166a8fb5a0
konstantin.cherkashin <konstantin.cherkashin@symmetrics.de>
2014-11-05 15:44:36 +0200

    init

---
+++ config/bindings.el
@@ -0,0 +1,30 @@
+;; Place your bindings here.
+
+;; For example:
+;;(define-key global-map (kbd "C-+") 'text-scale-increase)
+;;(define-key global-map (kbd "C--") 'text-scale-decrease)
+
+;; General
+
+(defvar my-keys-minor-mode-map (make-keymap) "my-keys-minor-mode keymap.")
+
+(define-key my-keys-minor-mode-map (kbd "M-;") 'execute-extended-command)
+(define-key my-keys-minor-mode-map (kbd "C-;") ctl-x-map)
+
+(define-minor-mode my-keys-minor-mode
+  "A minor mode so that my key settings override annoying major modes."
+  t " my-keys" 'my-keys-minor-mode-map)
+
+(my-keys-minor-mode 1)
+
+;; Unset key bindings from minibuffer
+(defun my-minibuffer-setup-hook ()
+  (my-keys-minor-mode 0))
+
+;; Speedbar
+
+(define-key global-map (kbd "C-c n t") 'sr-speedbar-toggle)
+
+;; Mingus
+
+(define-key global-map (kbd "C-c m p") 'mingus)
---
+++ init.el
@@ -0,0 +1,58 @@
+;; User pack init file
+;;
+;; Use this file to initiate the pack configuration.
+;; See README for more information.
+
+;;;;; Configs
+
+;; Load bindings config
+(live-load-config-file "bindings.el")
+;; Load secrets
+(live-load-config-file "secrets.el")
+
+;;;;; Libs
+
+(live-add-pack-lib "cider-nrepl")
+
+;; Circe (IRC)
+(live-add-pack-lib "circe/lisp")
+;; Mingus (mpd)
+(live-add-pack-lib "mingus")
+
+(require 'mingus)
+
+;;;;; IRC
+
+(autoload 'circe "circe" "Connect to an IRC server" t)
+
+;; connect to irc
+(defun irc ()
+  "start irc client"
+   (interactive)
+   (circe "Bitlbee")
+     (save-window-excursion
+       (sleep-for 1)
+       (set-buffer "localhost:6667")
+       (with-circe-server-buffer
+	 (biden)))
+     (switch-to-buffer "&bitlbee"))
+
+;; bitlbee identify
+(defun biden ()
+  "bitlebee identify"
+  (interactive)
+  (set-buffer "&bitlbee")
+  (circe-server-send
+   (format "PRIVMSG &bitlbee :identify %s"
+	   bitlbee-passwd)))
+
+;;;; Navigation
+
+;; Speedbar
+
+(require 'sr-speedbar)
+
+;; Speedbar goes left
+(setq sr-speedbar-right-side nil)
+;; Show all files
+(setq speedbar-show-unknown-files t)
